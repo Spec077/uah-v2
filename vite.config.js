44b1d246790fdc0c05ac38c64616d0ce8483e3c6
Spec077 <boluwatifeobasa830@gmail.com>
2026-07-12 02:26:17 +0100

    description preview fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: new URL('./index.html', import.meta.url).pathname,
+        careers: new URL('./careers.html', import.meta.url).pathname,
+      },
+    },
+  },
   plugins: [react()],
 })
